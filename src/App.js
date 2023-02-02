cd4220c6ccafa829447112a064911bd13ad89ddf
--replace-all <kirthibagrecha4@gmail.comfgit config --global user.email kirthibagrecha4@gmail.com>
2023-02-02 19:48:26 +0530

    Build LoginPage with Google sign-in

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
 import './App.css'
+import Login from './components/Login'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import Home from './components/Home'
+import Header from './components/Header'
 
 function App() {
-  return <h1>Hello, World!</h1>
+  return (
+    <div>
+      <Router>
+        <Switch>
+          <Route exact path={'/'} component={Login} />
+          <Route path={'/home'}>
+            <Header />
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
+    </div>
+  )
 }
-
 export default App
