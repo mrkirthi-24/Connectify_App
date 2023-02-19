2afe094190880bc8205365fc90f54a8970900cc2
mrkirthi-24 <kirthibagrecha4@gmail.com>
2023-02-19 12:04:53 +0530

    Added comments

--- src/store/index.js
+++ src/store/index.js
@@ -2,6 +2,10 @@ import { createStore, applyMiddleware } from "redux";
 import thunkMiddleware from "redux-thunk";
 import rootReducer from "../reducers";
 
+//here createStore is used to create new store |
+//rootReducer holds the initial state |
+//applyMiddleware is used to apply middleware function
+
 const store = createStore(rootReducer, applyMiddleware(thunkMiddleware));
 
 export default store;
